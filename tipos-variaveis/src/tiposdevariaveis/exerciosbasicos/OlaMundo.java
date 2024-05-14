c2bb98883dcd87034c92efac37e028333a71ec0f
hugomota5 <hugojcmota5@gmail.com>
2024-05-14 16:39:52 -0300

    exercicio basico 01

---
+++ tipos-variaveis/src/tiposdevariaveis/exerciosbasicos/OlaMundo.java
@@ -0,0 +1,10 @@
+package tiposdevariaveis.exerciosbasicos;
+
+public class OlaMundo {
+
+    // criando o método 
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+
+}
