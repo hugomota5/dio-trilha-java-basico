66b9f6acffbb6e436752892f6f9d7d6006beab16
hugomota5 <hugojcmota5@gmail.com>
2024-05-15 10:11:59 -0300

    operador uniario

---
+++ tipos-variaveis/src/operadores/Incremento.java
@@ -0,0 +1,14 @@
+package operadores;
+
+public class Incremento {
+   
+    public static void main(String[] args) {
+        int numero = 5;
+        // x repeticao
+        // numero = numero + 1;
+        numero++;
+        System.out.println(numero);
+        System.out.println(++ numero);
+        
+    }
+}
